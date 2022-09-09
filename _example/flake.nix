1989fd84917248aca2869d23d58ab32bde9c0297
Adrian Hesketh <adrianhesketh@hushmail.com>
2022-09-09 20:19:54 +0100

    feat: add build outputs for other architectures

--- _example/flake.nix
+++ _example/flake.nix
@@ -1,8 +1,8 @@
 {
   inputs = {
-    #agg.url = github:a-h/nix-asciinema-agg;
+    agg.url = github:a-h/nix-asciinema-agg;
     # For local development, use this instead.
-    agg.url = "path:../";
+    #agg.url = "path:../";
     flake-utils.url = "github:numtide/flake-utils";
     flake-utils.inputs.nixpkgs.follows = "nixpkgs";
   };
@@ -15,13 +15,12 @@
           };
       in
       {
-        default = agg.outputs.packages.${system}.agg;
         # Access this by `nix shell`
-        packages = agg.outputs.packages.${system}.agg;
+        defaultPackage = agg.outputs.packages.${system};
         # Access this via `nix develop`
         devShell = pkgs.mkShell {
           packages = [
-            agg.outputs.packages.${system}.agg
+            agg.outputs.packages.${system}
             pkgs.cowsay
           ];
         };
